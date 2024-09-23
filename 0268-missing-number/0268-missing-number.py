c3bb27df157e4d3cb66fe2e3eb98e0ca7719f3a4
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2024-09-24 02:56:23 +0530

    Time: 177 ms (39.07%), Space: 12.9 MB (69.92%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -5,20 +5,48 @@ class Solution(object):
         :rtype: int
         """
         # brute force worst solution posible
-        # min_n=0
-        # max_n=len(n)
-        # li=[x for x in range(min_n,max_n+1)]
-        # # print(li)
-        # for i in li:
+
+        # minn=0
+        # maxx=max(n)+1
+
+        # for i in range(maxx+1):
         #     if(i not in n):
-        #         print(i)
         #         return i
         # return 0
+        # TC=O^2
+        # SC=O(1)
 
-        minn=0
-        maxx=max(n)+1
+# ----------------------------
+# passed 110/122 great
 
-        for i in range(maxx+1):
-            if(i not in n):
-                return i
-        return 0
+        # sort arr first
+        # the run a loop if diff between i and i+1==1 continue
+        # else :
+        #     return i
+
+        # n.sort()
+        # move_backward=0
+        # for i in range(1,len(n)):
+        #     if(abs(n[move_backward]-n[i])==1):
+        #         move_backward+=1
+        #         continue
+        #     else:
+        #         return i
+        # if(len(n)!=1):        
+        #     return len(n)
+        # else:
+        #     for i in range(0,len(n)+1):
+        #         if(abs(n[move_backward]-n[i])==1):
+        #             move_backward+=1
+        #             continue
+        #         else:
+        #             return i
+
+
+# -------------------------------------
+# using a formula to calc sum as
+# sum of n numbers = (n(n+1))/2
+        lenn=len(n)
+        res=(lenn*(lenn+1)) // 2
+        summ=sum(n)
+        return res-summ
