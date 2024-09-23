a66f2a1c2ef6ef7a548e13593ddfd6a20c15d0be
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2024-09-24 02:32:22 +0530

    Time: 1451 ms (6.87%), Space: 13 MB (37.57%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -4,12 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        min_n=0
-        max_n=len(n)
-        li=[x for x in range(min_n,max_n+1)]
-        # print(li)
-        for i in li:
+        # brute force worst solution posible
+        # min_n=0
+        # max_n=len(n)
+        # li=[x for x in range(min_n,max_n+1)]
+        # # print(li)
+        # for i in li:
+        #     if(i not in n):
+        #         print(i)
+        #         return i
+        # return 0
+
+        minn=0
+        maxx=max(n)+1
+
+        for i in range(maxx+1):
             if(i not in n):
-                print(i)
                 return i
         return 0
