f4cbc4e001b016815e3dcc337b829aa97c06e4fb
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2024-09-24 01:36:13 +0530

    Time: 227 ms (20.1%), Space: 12.9 MB (29.85%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,37 +3,31 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
-        2 ptr approach 
-        if(n[l]==0):
-            temp=n[l]
-            n[l]=n[r]
-            n[l]=temp
-            l+=1
-            r-=1
-        else:
-            l+=1
         """
-        # l=0
-        # r=len(n)-1
+        #worst approach
+        # arr_len=len(n)-1
+        # new_li=[x for x in n if x!=0]
+        # # print(new_li)
+        # for i in range(len(new_li)-1,arr_len):
+        #     new_li.append(0)
+        # # print(new_li)
 
-        # while l<r:
-        #     if(n[l]==0):
-        #         print("ok")
-        #         temp=n[l]
-        #         n[l]=n[r]
-        #         n[r]=temp
-        #         l+=1
-        #         r-=1
-        #     else:
-        #         l+=1
-        # print(n)
+        # for i in range(0,len(new_li)):
+        #     n[i]=new_li[i]
 
-        arr_len=len(n)-1
-        new_li=[x for x in n if x!=0]
-        # print(new_li)
-        for i in range(len(new_li)-1,arr_len):
-            new_li.append(0)
-        # print(new_li)
 
-        for i in range(0,len(new_li)):
-            n[i]=new_li[i]
+        count_zeros=0
+        move_backward=0
+
+        for i in range(len(n)):
+            if n[i]!=0:
+                n[move_backward]=n[i]
+                move_backward+=1
+            else:
+                count_zeros+=1
+        k=move_backward-1      
+        i=len(n)-1
+        while k!=i:
+            n[i]=0
+            i-=1
+        print(n)
