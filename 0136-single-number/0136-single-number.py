c030c9eb41219ba24fa692d244b0bcc2612a7c0f
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2024-09-24 19:45:54 +0530

    Time: 198 ms (26.23%), Space: 13.4 MB (98.84%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        using bit wise operator ^ xor
+        5^5=5 but if 5^6 op will be diff so we can easily use it to 
+        identify diff element from an array
+        """
+        res_xor=0 
+        for i in nums:
+            res_xor^=i
+        return res_xor
+
+        
