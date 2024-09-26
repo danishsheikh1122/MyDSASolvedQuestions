9c0b27f319cbc3ddb13f7f1a4d0247e1b1b0e923
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2024-09-26 14:30:29 +0530

    Time: 40 ms (70.91%), Space: 11.8 MB (43.64%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -9,25 +9,63 @@ class Solution(object):
         create a dict of rn
         and check if magazine has att that chars
         """
-        di_r={}
-        di_m={}
+        # not very good algorithm
+        # di_r={}
+        # di_m={}
+        # for i in ransomNote:
+        #    di_r[i] = di_r[i] + 1 if i in di_r else 1
+        # for i in magazine:
+        #     if(i in di_r):
+        #         di_m[i] = di_m[i] + 1 if i in di_m else 1
+
+        # print(di_r)
+        # print(di_m)
+        # res=False
+        # for key,value in di_r.items():
+        #     if(key in di_m and di_m[key]>=value):
+        #         print(di_m[key])
+        #         res=True
+        #     else:
+        #         return False
+        # return res
+        # above solution is not very good
+        # tc-o(n)
+        # sc-o(k)worst
+        # -----------------------------------
+        # use pop or del to remove an element from dictionary
+        # di={}
+        # for c in magazine:
+        #     if c in ransomNote:
+        #         di[c]=di[c]+1 if c in di else 1
+        # print(di)
+        # res=True
+        # if(len(di)>0):
+        #     for c in ransomNote:
+        #         if c in di and di[c]>0:
+        #             di[c]-=1
+        #         else:
+        #             del di[c]
+
+        # else:
+        #     return False
+            
+        # wrong logic again below
+        # -----------------------------------
+        di={}
+        for c in magazine:
+            # if c in ransomNote:
+                di[c]=di[c]+1 if c in di else 1
+        print(di)
         for i in ransomNote:
-           di_r[i] = di_r[i] + 1 if i in di_r else 1
-        for i in magazine:
-            if(i in di_r):
-                di_m[i] = di_m[i] + 1 if i in di_m else 1
-
-        print(di_r)
-        print(di_m)
-        res=False
-        for key,value in di_r.items():
-            if(key in di_m and di_m[key]>=value):
-                print(di_m[key])
-                res=True
-            else:
+            if i not in di:
                 return False
-        return res
-
-
+            elif di[i]==1:
+                del di[i]
+            else:
+                di[i]-=1
+        return True
 
         
+
+
+
