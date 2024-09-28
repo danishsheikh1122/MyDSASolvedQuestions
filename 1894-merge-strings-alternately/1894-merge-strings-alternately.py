fe6f7f2f181ec157780770f075177b6a253e6c52
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2024-09-28 13:51:18 +0530

    Time: 17 ms (34.74%), Space: 11.5 MB (98.69%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -6,19 +6,41 @@ class Solution(object):
         :rtype: str
         """
         # s=word2[:len(word1)]
-        minW=word1 if len(word1)<len(word2) else word2
-        maxW=word1 if len(word1)>len(word2) else word2
-        l=len(minW)
-        print(l)
+        # --------------------------------
+        # another 2 ptr approach
+        # minW=word1 if len(word1)<len(word2) else word2
+        # maxW=word1 if len(word1)>len(word2) else word2
+        # l=len(minW)
+        # print(l)
 
-        resS=""
-        r=0
-        while r!=l:
-            resS=resS+word1[r]+word2[r]
-            r+=1
-        resS+=maxW[r:]
-        return resS
+        # resS=""
+        # r=0
+        # while r!=l:
+        #     resS=resS+word1[r]+word2[r]
+        #     r+=1
+        # resS+=maxW[r:]
+        # return resS
 
+        # Tc on 
+        # sc on
+        # make it more optimal
+
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        result = []
+        i = 0
+        while i < len(word1) or i < len(word2):
+            if i < len(word1):
+                result.append(word1[i])
+            if i < len(word2):
+                result.append(word2[i])
+            i += 1
+        return ''.join(result)
             
 
         
