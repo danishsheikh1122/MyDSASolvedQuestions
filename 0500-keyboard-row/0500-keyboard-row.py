6c1f827afef9921b405e8a791ad771545812b787
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2024-09-30 22:57:17 +0530

    Time: 15 ms (47.68%), Space: 11.6 MB (84.26%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def findWords(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        bruteforce is to create 3 str and check if arr belongs to anyone 
+        else
+
+        """
+        
+        di={1:'qwertyuiop',
+            2:'asdfghjkl',
+            3:'zxcvbnm'}
+        res_li=[]
+        index=0
+        for w in words:
+            w=[x.lower() for x in w]
+            
+            row=0
+            if w[0] in di[1]:
+                row+=1
+            elif w[0] in di[2]:
+                row+=2
+            elif w[0] in di[3]:
+                row+=3
+            l=len(w)
+            i=0
+            for char in w:
+                print(row)
+                if char in di[row]:
+                    i+=1
+                if(i == l):
+                    res_li.append(words[index])
+
+            index+=1
+
+        # print(res_li)
+        return res_li
