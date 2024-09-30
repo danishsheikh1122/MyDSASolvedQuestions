a0080bd8660df89a524627d04ae0e5cef53879c7
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2024-10-01 01:08:01 +0530

    Time: 170 ms (57.29%), Space: 14.2 MB (7.69%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -23,23 +23,33 @@ class Solution(object):
         
         # from collections import Counter
 
-        # di=Counter(nums)
-        # for key,value in di.items():
-        #     if value>1:
-        #         print(key,value)
-        #         if di[key-1]==0 and key-1!=0:
-        #             return [key,key-1]
-        #         else:
-        #             return [key,k]
+        di=Counter(nums)
+        dup=missing=None
+        for key,value in di.items():
+            if value>1:
+                dup=key
+        index=1
+        for key in range(1,len(nums)+1):
+            if(di[key]==0):
+                missing=key
+                return [dup,missing]
+            index+=1
+
+                # print(key,value)
+                # if di[key-1]==0 and key-1!=0:
+                #     return [key,key-1]
+                # else:
+                #     return [key,k]
+                
 
 
           
-        for i in range(1, len(nums) + 1):
-            count = nums.count(i)
-            if count == 2:
-                dup = i
-            elif count == 0:
-                missing = i
+        # for i in range(1, len(nums) + 1):
+        #     count = nums.count(i)
+        #     if count == 2:
+        #         dup = i
+        #     elif count == 0:
+        #         missing = i
         
-        return [dup, missing]
+        # return [dup, missing]
     
