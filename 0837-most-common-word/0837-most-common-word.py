1e04972ca3d6a61a09e1a0415d962d0d21ed10ba
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2024-10-01 02:37:53 +0530

    Time: 20 ms (56.91%), Space: 11.7 MB (80.2%) - LeetHub

--- 0837-most-common-word/0837-most-common-word.py
+++ 0837-most-common-word/0837-most-common-word.py
@@ -1,4 +1,41 @@
-# from collections import Counter
+# class Solution(object):
+#     def mostCommonWord(self, p, banned):
+#         """
+#         :type paragraph: str
+#         :type banned: List[str]
+#         :rtype: str
+#         """
+
+#         special_chars = "!@#$%^&*()_+-={}[]|\\:;\"'<>,.?/~"
+#         for char in special_chars:
+#             p = p.replace(char, ' ')
+#         print(p)
+#         # below step can be simplified
+#         # p=p.split(' ')
+#         # p=[c.lower() for c in p]
+#         p=p.lower().split()
+#         di=Counter(p)
+#         print(di)
+
+#         banned.append('')
+#         for word in banned:
+#             del di[word]
+
+#         # di=dict(sorted(di.items(), key=lambda item: item[1]))
+#         # print(di)
+#         # not working
+
+#         maxx=0
+#         ans=''
+#         for key,value in di.items():
+#             if maxx<value:
+#                 maxx=value
+#                 ans=key
+#         return ans
+#         # worst solution 
+#         # TC on 
+#         # SC on
+from collections import Counter
 
 class Solution(object):
     def mostCommonWord(self, p, banned):
@@ -7,7 +44,7 @@ class Solution(object):
         :type banned: List[str]
         :rtype: str
         """
-
+        
         # Remove specific special characters
         for char in "!?',;.":
             p = p.replace(char, ' ')
