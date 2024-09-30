25c8ae4348c68266226aca7b8fbe48ee5505c26f
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2024-09-30 17:01:27 +0530

    Time: 73 ms (39.09%), Space: 13.3 MB (40.44%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -4,9 +4,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        index=1
+        # index=1
+        # for i in range(1,len(nums)):
+        #     if(nums[i]!=nums[i-1]):
+        #         nums[index]=nums[i]
+        #         index+=1
+        # return index      
+
+
+
+        # using 2 pointers approach
+
+
+        # i,j i will move from starting and j will act as an index of last uncommon and 
+        # i will be as incrementation
+
+        j=1
+
         for i in range(1,len(nums)):
             if(nums[i]!=nums[i-1]):
-                nums[index]=nums[i]
-                index+=1
-        return index      
+                nums[j]=nums[i]
+                j+=1
+
+        print(nums)
+        return j
