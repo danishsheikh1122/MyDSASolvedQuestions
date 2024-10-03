c47a9be0145fac4fdbf562f704cb74f27a9b687f
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2024-10-03 20:06:22 +0530

    Time: 95 ms (38.66%), Space: 16.9 MB (16.76%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,40 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        for i in range(0,9):
+            check_dups=set()
+            # print(i)
+            for j in range(0,9):
+                temp=board[i][j]
+                if temp in check_dups:
+                    return False
+                elif temp is not '.':
+                    check_dups.add(temp)
+
+                    
+
+        for i in range(0,9):
+            check_dups=set()
+            # print(i)
+            for j in range(0,9):
+                temp=board[j][i]
+                if temp in check_dups:
+                    return False
+                elif temp is not  '.':
+                    check_dups.add(temp)
+
+
+        to_iterate=[(0,0),(0,3),(0,6),(3,0),(3,3),(3,6),(6,0),(6,3),(6,6)]
+
+        for i,j in to_iterate:
+            sett=set()
+            for row in range(i,i+3):#exclusive
+                for column in range(j,j+3):
+                    if board[row][column] in sett :
+                        return False
+                    elif board[row][column] not in ".":
+                    # elif board[row][column] is not ".":
+                        sett.add(board[row][column])
+
+
+        return True
