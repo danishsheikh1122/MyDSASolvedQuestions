ba5a1c19725258b3ced0e5a7b7a03130c06cbe0e
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2024-10-04 23:06:55 +0530

    Time: 2818 ms (6.41%), Space: 17.5 MB (61.36%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,19 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[j] == target - nums[i]:
-                    return [i, j]
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        l=0
+        r=len(nums)-1
+
+        while l<r:
+            if(nums[l]+nums[r]==target):
+                return [l,r]
+            elif((l+1)==r):
+                l+=1
+                r=len(nums)-1
+            else:
+                r-=1
+        print(nums)
