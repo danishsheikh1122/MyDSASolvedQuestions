9f467ed23d45aa042c8d503c632e4025e347b31d
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2024-10-05 16:40:00 +0530

    Time: 3 ms (99.58%), Space: 11.6 MB (68.21%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -21,13 +21,29 @@ class Solution(object):
         # ---------------------------------------
         # top down -memo -dp
 
-        memo={0:1,1:1}
+        # memo={0:1,1:1}
 
-        def rec(n):
-            if n in memo:
-                return memo[n]
-            else:
-                memo[n]=rec(n-1)+rec(n-2)
-                return memo[n]
-        return rec(n)
+        # def rec(n):
+        #     if n in memo:
+        #         return memo[n]
+        #     else:
+        #         memo[n]=rec(n-1)+rec(n-2)
+        #         return memo[n]
+        # return rec(n)
 
+        # still tc-on and sc-on
+        # not efficient
+
+
+        # bottom up
+
+
+        if n ==0 or n==1:
+            return 1
+
+        arr=[0]*(n+1)
+        arr[0]=1
+        arr[1]=1
+        for i in range(2,n+1):
+            arr[i]=arr[i-2]+arr[i-1]
+        return arr[n]
