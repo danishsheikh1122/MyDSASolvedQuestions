497103673ee1faa3e0bfcd4080f742fb93cac01f
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2024-10-05 16:21:42 +0530

    Time: 8 ms (92.39%), Space: 11.7 MB (33.35%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -7,14 +7,38 @@ class Solution(object):
 
         # usnig memo->top-down dp
 
-        memo={0:0,1:1}
+        # memo={0:0,1:1}
 
-        def recursion(n):
-            if n in memo:
-                return memo[n]
-            else:
-                memo[n]=recursion(n-2)+recursion(n-1)
-                return memo[n]
+        # def recursion(n):
+        #     if n in memo:
+        #         return memo[n]
+        #     else:
+        #         memo[n]=recursion(n-2)+recursion(n-1)
+        #         return memo[n]
             
-        return recursion(n)
+        # return recursion(n)
+
+        # on
+        # on
+
+        # -----------------------------------------------------
+
+        # bottom up -dp easy peasy :))
+        if n==0:
+            return 0
+
+        if n==1:
+            return 1
+
+        arr=[0]*(n+1)
+        arr[0]=0
+        arr[1]=1
+
+        for i in range(2,n+1):
+            arr[i]=arr[i-2]+arr[i-1]
+        return arr[n]
+
+
+
+
         
