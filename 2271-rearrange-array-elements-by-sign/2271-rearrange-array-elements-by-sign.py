e364dc17e076d7f2cab00961b570a65d025d7408
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2024-10-07 01:33:50 +0530

    Time: 1202 ms (34.83%), Space: 42.2 MB (96.81%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
@@ -4,16 +4,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        # brute force approach is to make 2 seperate li of +ve and -ve and run a loop to 
+        # BRUTE FORCE approach is to make 2 seperate li of +ve and -ve and run a loop to 
         # make final res
+
+        # pos=[x for x in nums if x>=0]
+        # neg=[x for x in nums if x<0]
+        # res_li=[]
+        # i=0
+        # while i!=len(pos):
+        #     res_li.append(pos[i])
+        #     res_li.append(neg[i])
+        #     i+=1
+        # # print(res_li)
+        # return res_li
+        # tc-on sc-on
+        # -----------------------------------
+        # same approach but kind of in place
+
 
         pos=[x for x in nums if x>=0]
         neg=[x for x in nums if x<0]
-        res_li=[]
-        i=0
-        while i!=len(pos):
-            res_li.append(pos[i])
-            res_li.append(neg[i])
-            i+=1
-        # print(res_li)
-        return res_li
+        for i in range(0,len(nums)/2):
+            nums[i*2]=pos[i]
+            nums[i*2+1]=neg[i]
+        return nums
