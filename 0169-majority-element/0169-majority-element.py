acee9f3f2fd4af3db45ad57a20e3dc180556ed00
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2024-10-08 11:50:22 +0530

    Time: 164 ms (57.58%), Space: 13.7 MB (15.04%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,6 +1,6 @@
 import math 
 class Solution(object):
-    def majorityElement(self, n):
+    def majorityElement(self, nums):
         """
         :type nums: List[int]
         :rtype: int
@@ -21,24 +21,51 @@ class Solution(object):
 
         # this code gives memory limit exceed so another method
 
-# ----------------------------------------------------------------------
+# ----------------------------------------------------------------------    
         # using hash table ds as a DICTIONARY
-
+        # tc-sc-on
         # from collections import defaultdict
 
-        di=defaultdict(int)
-        # print(di[0])
+        # di={}
+        # # print(di[0])
 
-        for i in n:
-            di[i]+=1
-        # print(di)
+        # for i in n:
+        #     di[i]=di[i]+1 if i in di else 1
+        # # print(di)
 
-        for key,value in di.items():
-            if value>len(n)//2:
-                return key
-        return 0
+        # for key,value in di.items():
+        #     if value>len(n)//2:
+        #         return key
+        # return 0
+
+        # ------------------------------------------
+        # USE MOORES VOTING ALGO TO SOLVE THIS
 
 
+        # MOORES ALGO TO FIND MAJORITY ELEM FROM AN ARR
+
+        # TC ON -SC O(1)
+
+        mj_elem=0
+
+        count=0
+
+        for i in range(len(nums)):
+            if count==0:
+                count+=1
+                mj_elem=nums[i]
+            elif nums[i]==mj_elem:
+                count+=1
+            else:
+                count-=1
+        count=0
+        for i in nums:
+            if i ==mj_elem:
+                count+=1 
+        if count>len(nums)/2:
+            return mj_elem
+        else:
+            return nums
 
 
 
