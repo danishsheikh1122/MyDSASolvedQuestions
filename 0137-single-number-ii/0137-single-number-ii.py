3c39da569c9ec2a5a6bec3a3bcb63a3c81bd6dd8
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2024-10-09 01:36:52 +0530

    Time: 643 ms (18.38%), Space: 13.4 MB (87.31%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -7,17 +7,23 @@ class Solution(object):
 
         # Brute Force
 
-        di={}
-        for i in nums:
-            di[i]=di[i]+1 if i in di else 1
+        # di={}
+        # for i in nums:
+        #     di[i]=di[i]+1 if i in di else 1
 
-        for key,value in di.items():
-            if value==1:
-                return key
+        # for key,value in di.items():
+        #     if value==1:
+        #         return key
         # TC-On-pass 
         # SC-on
         # ------------------------------------
-        # Better solution 
+        # Better solution with built in array-function
         for i in nums:
             if nums.count(i)==1:
                 return i
+        #  Tc-on 1 pass
+        #  sc-o1
+        # ----------------------------------------
+        # optimal solution
+
+        
