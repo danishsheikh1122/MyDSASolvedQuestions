817157641e68875590a9fd8399225fb767d53af2
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2024-10-08 20:16:39 +0530

    Time: 878 ms (54.19%), Space: 14.7 MB (94.82%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -26,6 +26,7 @@ class Solution(object):
         for i in range(0,len(n)):
             l=i+1
             r=len(n)-1
+            # this if makes code faster if i ==i-1 then same number so move to non similar number 
             if i>0 and n[i]==n[i-1]:
                 continue
             while l<r:
@@ -35,6 +36,9 @@ class Solution(object):
                     res.add((n[i],n[l],n[r]))
                     l+=1
                     r-=1
+
+                    # this 2 while loops makes the code fast 
+                    # if same elem appear then it will jumn to next non similar  number
                     while l<r and n[l]==n[l-1]:
                         l+=1
                     while l<r and n[r]==n[r+1]:
