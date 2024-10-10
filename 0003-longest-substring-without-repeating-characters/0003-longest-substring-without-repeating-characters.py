faec1b62e8092b891c769899a020fbf29b534c32
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2024-10-11 01:52:35 +0530

    Time: 45 ms (34.06%), Space: 11.7 MB (97.98%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        # passes 407/1000
+        # res_s=""
+        # max_len=0
+        # for char in s:
+        #     if char not in res_s:
+        #         res_s+=char
+        #         max_len=max(max_len,len(res_s))
+        #     else:
+        #         res_s=""
+        #         res_s+=char
+        # return max_len
+        
+        # -----------------------------------------
+        # sliding window approach 
+
+        max_len=l=r=0
+        di={}
+
+        while r<len(s):
+
+            if s[r] in di:
+                if di[s[r]]>=l:
+                    l=di[s[r]]+1
+            
+            max_len=max(max_len,r-l+1)
+            di[s[r]]=r
+            r+=1
+        print(di,max_len)
+        return max_len
+
+
+
