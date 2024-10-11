cfd2fae9ee9c46236c12bdaebe5d79771c3bbd88
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2024-10-11 21:53:43 +0530

    Time: 465 ms (35.3%), Space: 12 MB (88.84%) - LeetHub

---
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def longestOnes(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        # make this prob as find the max_len of with at most k zeros
+        
+
+        # sliding window
+        l=r=max_len=no_zeros=0
+
+        while r<len(nums):
+            if nums[r]==0:
+                no_zeros+=1
+            while no_zeros>k:
+                if nums[l]==0:
+                    no_zeros-=1
+                l+=1
+            if no_zeros<=k:
+                max_len=max(max_len,r-l+1)
+            r+=1
+        return max_len
+
+        
