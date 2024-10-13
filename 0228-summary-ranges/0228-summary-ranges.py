1513c6248fcf61db0571844e70f1ede1f5e5b74c
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2024-10-14 01:05:50 +0530

    Time: 13 ms (57.69%), Space: 11.6 MB (77.79%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def summaryRanges(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[str]
+        """
+
+        res_li=[]
+        i=0
+        while i < len(nums):
+            start=nums[i]
+            while i < len(nums)-1 and nums[i]+1 == nums[i+1]:
+                i+=1
+            if start!=nums[i]:
+                res_li.append(str(start)+"->"+str(nums[i]))
+            else:
+                res_li.append(str(nums[i]))
+
+            i+=1
+        return res_li
+
+
+
+        
