9b4fa0cf3a6034e1fd614d4bd45946226b83b5e6
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2024-10-14 02:02:38 +0530

    Time: 42 ms (34.38%), Space: 12.6 MB (49.69%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.py
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -4,18 +4,33 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        res_li=[]
-        di=Counter(nums)
-        if(di):
-            res_li.append(max(di))
-            di.pop(max(di))
+        # Extreme Brute Force
+        # res_li=[]
+        # di=Counter(nums)
+        # if(di):
+        #     res_li.append(max(di))
+        #     di.pop(max(di))
 
-        if(di):
-            res_li.append(max(di))
-            di.pop(max(di))
-        if(di):
-            res_li.append(max(di))
+        # if(di):
+        #     res_li.append(max(di))
+        #     di.pop(max(di))
+        # if(di):
+        #     res_li.append(max(di))
 
-        if len(res_li)!=3:
-            return max(res_li)
-        return res_li[-1]
+        # if len(res_li)!=3:
+        #     return max(res_li)
+        # return res_li[-1]
+
+        # Tc-on
+        # sc-on
+
+        # ---------------------------------------------
+
+        # first sort the nums and then count if len>=3 then print -3 else max of li
+        nums=list(set(nums))
+        nums.sort()
+        if len(nums)>=3:
+            print(nums[-3])
+            return nums[-3]
+
+        return max(nums)
