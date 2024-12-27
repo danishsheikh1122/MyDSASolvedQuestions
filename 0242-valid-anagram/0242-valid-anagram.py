a67f3b5d1526752f96d115c4b934ead41a1ea499
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2024-12-27 14:26:32 +0530

    Time: 16 ms (71.43%), Space: 12.5 MB (61.4%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -4,56 +4,17 @@ class Solution(object):
         :type s: str
         :type t: str
         :rtype: bool
-        create a counter and count every word and just check if every char is   
-        present or not 
         """
-        counter={}
-        for char in s:
-            counter[char]=counter[char]+1 if char in counter else 1
         if len(s)!=len(t):
             return False
-        for char in t:
-            if char not in counter:
-                return False
-            elif counter[char]==1:
-                del counter[char]
+        di={}
+        for c in s:
+            di[c]=di[c]+1 if c in di else 1
+        print(di)
+        for c in t:
+            if c in di and di[c]>0:
+                di[c]-=1
             else:
-                counter[char]-=1
+                return False
+        
         return True
-
-        #Tc-ON
-        #Sc-ON
-        # best in 29 s beats 77 
-        # more faster algorithm but it can be done in less code lets see
-
-        # ---------------------------------------------------
-        # 2 approach   
-        # s=sorted(s)
-        # t=sorted(t)
-        # # print(s,t)
-        # if(len(s)!=len(t)):
-        #     return False
-        # for i in range(len(s)):
-        #     if s[i]==t[i]:
-        #         continue
-        #     else:
-        #         return False
-        # return True
-
-        # not as fast as previous one
-        # Tc-ON Sc-ON
-
-        # -----------------------------------
-        # another fast approach
-        # from collections import Counter
-
-        # if(len(s)!=len(t)):
-        #     return False
-        # s=Counter(s)
-        # t=Counter(t)    
-
-        # if s==t:
-        #     return True    
-        # tc-on sc-on
-
-        #  first soultion is best
