90d215ab970a59d7f9ec3229b9cc7a5c9b591d5c
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2024-12-27 15:15:32 +0530

    Time: 7 ms (45.23%), Space: 12.4 MB (11.28%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
@@ -3,18 +3,9 @@ class Solution(object):
         """
         :type text: str
         :rtype: int
-        first count the no of bollons and store in hash map 
-
         """
+        s="balloon"
         di=defaultdict(int)
-
-        strr=set('balloon')
-
-        for char in text:
-            if char in strr:
-                di[char]+=1
-        print(di)
-        
-
+        for c in text:
+                di[c]=di[c]+1 if c in di else 1
         return min(di['b'],di['a'],di['l']//2,di['o']//2,di['n'])
-        
