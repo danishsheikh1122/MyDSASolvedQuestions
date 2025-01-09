dc582a47905f9f992e2cdf16c855538583b49cd2
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-01-09 23:54:15 +0530

    Time: 13 ms (35.34%), Space: 15.2 MB (5.9%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        # --------------------------------------
+        # # check len
+        # if len(s)!=len(t):
+        #     return False
+        # di={}
+        # for i in range(len(s)):
+        #     if s[i] not in di and t[i] not in di: 
+        #         di[s[i]]=t[i]
+        #     elif di[s[i]]==t[i]:
+        #         continue
+        #     else:
+        #         return False
+        # return True
+        # FAILED39/46 
+        # -------------------------------------------
+        # use 2 dic for forward and reverse mapping 
+        if len(s)!=len(t):
+            return False
+        dist,dits={},{}
+        for c1,c2 in zip(s,t):
+            if ((c1 in dist and dist[c1]!=c2) or (c2 in dits and dits[c2]!=c1)):
+                return False
+            dist[c1]=c2
+            dits[c2]=c1
+        return True
+
+                                                                      
