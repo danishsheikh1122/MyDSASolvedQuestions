98014843b8ab798f0da38e44f578dff47cd73cad
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-01-09 19:57:27 +0530

    Time: 0 ms (100%), Space: 12.6 MB (8.09%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -4,9 +4,8 @@ class Solution(object):
         :type nums1: List[int]
         :type nums2: List[int]
         :rtype: List[int]
-        create one dict for n1 and check i nums2 is present or ot 
         """
-        # from collections import Counter
+          # from collections import Counter
         # counter=Counter(nums1)
         # print(counter)
         # li=[]
@@ -38,10 +37,31 @@ class Solution(object):
         #------------------------------ 
         # another approach
 
-        li=set(nums1)
-        res_li=[]
-        for i in nums2:
-            if i in li and i not in res_li:
-                res_li.append(i)
-        return res_li
-    
+        # li=set(nums1)
+        # res_li=[]
+        # for i in nums2:
+        #     if i in li and i not in res_li:
+        #         res_li.append(i)
+        # return res_li
+        #------------------------------ 
+        # another approach but requires sorting of both the array first
+        # TC-on SC-on
+        arr=[]
+        x=y=0
+        nums1.sort()
+        nums2.sort()
+        while x<len(nums1) and y<len(nums2) :
+            if nums1[x]==nums2[y] :
+                if nums1[x] not in arr:
+                    arr.append(nums1[x])
+                x+=1
+                y+=1
+            elif nums1[x]!=nums2[y]:
+                if nums1[x]<nums2[y]:
+                    x+=1
+                else:
+                    y+=1
+        print(x,y)
+        return arr
+
+        
