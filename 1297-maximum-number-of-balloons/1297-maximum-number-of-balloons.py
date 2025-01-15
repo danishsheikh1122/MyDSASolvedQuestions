116aaa10cb53d1876884b79be6e6a5e1675d22eb
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-01-16 02:16:35 +0530

    Time: 0 ms (100%), Space: 12.5 MB (35.27%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
@@ -5,7 +5,7 @@ class Solution(object):
         :rtype: int
         """
         s="balloon"
-        di=defaultdict(int)
+        di=defaultdict(int)#it initializes every key with 0 value 
         for c in text:
                 di[c]=di[c]+1 if c in di else 1
         return min(di['b'],di['a'],di['l']//2,di['o']//2,di['n'])
