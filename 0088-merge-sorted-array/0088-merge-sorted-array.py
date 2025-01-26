bd0e360f8ff45e5e02c499be1a18e3279f597c19
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-01-26 21:43:39 +0530

    Time: 30 ms (5.19%), Space: 11.6 MB (100%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        l_nums2=0
+        for i in range(len(nums1)):
+            if(i>=m and nums1[i]==0):
+                nums1[i]=nums2[l_nums2]
+                l_nums2+=1
+                # print(l_nums2)
+        # print(nums1)
+        nums1.sort()
+
+        
