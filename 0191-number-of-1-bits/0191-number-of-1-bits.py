ed7b8289b2d7a9e59a205dd4b15707f606ac8dc3
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-01-26 21:46:39 +0530

    Time: 9 ms (5.06%), Space: 12.5 MB (6.06%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -4,21 +4,10 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        # n=bin(n)
+        n=bin(n)
 
-        # from collections import Counter
-        # ct=Counter(n)
-        # print(ct['1'])
-        # return ct['1']
-
-        # above is brute force and worst appoach 
-        # n-=1
-        # print(n & n)
+        from collections import Counter
+        ct=Counter(n)
+        print(ct['1'])
+        return ct['1']
         
-        # basic idea is to make n 0 and while making it zero inc ans
-
-        res=0
-        while n!=0:
-            res+=1
-            n=n & n-1
-        return res 
