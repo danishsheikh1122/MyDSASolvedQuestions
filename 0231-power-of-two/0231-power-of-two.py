e5f9b2b06bee433f57f907f55afb758b83f2623d
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-02-02 01:00:46 +0530

    Time: 0 ms (100%), Space: 12.4 MB (36.63%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        # just minus n-1 and and it with n
+        # if 2->0010 and 2-1=0001
+        # now perform logical anding 
+        # 0010&0001 ->1&1=1 else answer will be 0
+        if n==0:
+            return False
+        res=True if  (n & (n-1) ) ==0  else False
+        return res
+
+
