7f905dd99d3a46c361744cd02a60fe7dc2f6c9f9
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-02-03 20:03:32 +0530

    Time: 0 ms (100%), Space: 12.5 MB (42.13%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
@@ -6,6 +6,10 @@ class Solution(object):
         :rtype: int
         """ 
         temp_res=start^goal
-        res=bin(temp_res)[2:]
-
-        return sum(1 for bit in res if bit == '1')
+        count=0
+        # now run a loop while temp-_res and check if lsb & 1 is 1 the count and + else right shift the temo_res until i becomes 0 the loop will continues too run
+        while temp_res:
+            if temp_res&1:
+                count+=1
+            temp_res>>=1
+        return count
