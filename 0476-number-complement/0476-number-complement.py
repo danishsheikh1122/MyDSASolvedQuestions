cab7ed465940940e6b7be4b1811b37f804ab6261
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-02-04 02:00:42 +0530

    Time: 7 ms (3.09%), Space: 12.4 MB (72.63%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findComplement(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        # just brute force
+        li=[]
+        while num:
+            if (num&1):
+                li.append(0)
+            else:
+                li.append(1)
+            num>>=1
+        print(li)
+        li=li[::-1]
+        strr=''
+        for char in li:
+            strr+=str(char)
+        return int(strr,2)
+
