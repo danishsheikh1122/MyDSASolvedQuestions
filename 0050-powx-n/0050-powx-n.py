9d68dea3db8fa043362363f8017d54a766eba81b
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-02-03 22:19:29 +0530

    Time: 0 ms (100%), Space: 12.5 MB (21.06%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        # Fuck brute force
+        return pow(x,n)
