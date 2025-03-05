d098edb6712e24faa474bd1010eb6e6e65bbedef
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-03-05 19:57:50 +0530

    Time: 95 ms (16.44%), Space: 12.8 MB (76.64%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
@@ -5,38 +5,14 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        # make this prob as find the max_len of with at most k zeros
-        
-
-        # sliding window
-        # l=r=max_len=no_zeros=0
-
-        # while r<len(nums):
-        #     if nums[r]==0:
-        #         no_zeros+=1
-        #     while no_zeros>k:
-        #         if nums[l]==0:
-        #             no_zeros-=1
-        #         l+=1
-        #     if no_zeros<=k:
-        #         max_len=max(max_len,r-l+1)
-        #     r+=1
-        # return max_len
-        # tc-2on still we can make it more optimal
-        # --------------------------------------
-
-        l=r=max_len=zero=0
-
+        maxx=l=r=zeros=0
         while r<len(nums):
             if nums[r]==0:
-                zero+=1
-            if zero>k:
-                if nums[l]==0:
-                    zero-=1
-                l+=1
-            if zero<=k:
-                max_len=max(max_len,r-l+1)
+                zeros+=1
+                while zeros>k:
+                    if nums[l]==0:
+                        zeros-=1
+                    l+=1
+            maxx=max(maxx,r-l+1)
             r+=1
-        return max_len
-
-        # tc- 1N and sc o1
+        return maxx
