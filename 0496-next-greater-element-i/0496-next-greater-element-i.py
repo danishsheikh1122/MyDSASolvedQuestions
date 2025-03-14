944992886e288e213d809598ca7b163f96824948
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-03-14 16:36:52 +0530

    Time: 26 ms (37.37%), Space: 12.6 MB (53.39%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -25,3 +25,25 @@ class Solution(object):
                 if not found:
                     li.append(-1)
         return li
+        #  Time Complexity: O(m + k * n) or O(m^2) in the worst case. - Space Complexity: O(m + k).
+
+        # -------------------------------------------
+        # now using monotonic stack to solve this problem 
+        # below solution is not for this problem 
+        # nge=[]
+        # m_stack=[]
+        # top=-1
+
+        # for i in range(len(arr),-1,-1):
+        #     while top!=-1 and m_stack<=arr[i]:
+        #         m_stack.pop()
+        #     if top==-1:
+        #         nge[i]=-1
+        #     else:
+        #         nge[i]=m_stack[top]
+        #     m_stack.append(arr[i])
+        #     top+=1
+        # return nge
+
+
+        # new solution o(n+m) neetcode ----->
