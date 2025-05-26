746ddbed5eecdad7d4079e5b63fe0421ab55fa06
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-05-26 14:22:55 +0530

    Time: 11 ms (89.15%), Space: 12.5 MB (53.16%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -5,43 +5,32 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
-        # s=word2[:len(word1)]
-        # --------------------------------
-        # another 2 ptr approach
-        # minW=word1 if len(word1)<len(word2) else word2
-        # maxW=word1 if len(word1)>len(word2) else word2
-        # l=len(minW)
-        # print(l)
-
-        # resS=""
-        # r=0
-        # while r!=l:
-        #     resS=resS+word1[r]+word2[r]
+        # res_str=""
+        # i=l=r=0
+        # l1=len(word1)
+        # l2=len(word2)
+        # while i<min(l1,l2):
+        #     res_str+=word1[l]
+        #     res_str+=word2[r]
+        #     l+=1
         #     r+=1
-        # resS+=maxW[r:]
-        # return resS
+        #     i+=1
 
-        # Tc on 
-        # sc on
-        # make it more optimal
-
-class Solution(object):
-    def mergeAlternately(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: str
-        """
-        result = []
-        i = 0
-        while i < len(word1) or i < len(word2):
-            if i < len(word1):
-                result.append(word1[i])
-            if i < len(word2):
-                result.append(word2[i])
-            i += 1
-        return ''.join(result)
-            
-
+        # res_str=res_str+word1[l:] if l1>l2 else res_str+word2[r:] 
+        # print(res_str)
+        # return res_str
         
-        
+        min_len=min(len(word1),len(word2))
+        new_str=""
+        for i in range(min_len):
+            new_str+=word1[i]
+            new_str+=word2[i]
+        new_str+=word1[min_len:] if len(word1)>len(word2) else word2[min_len:]
+        # print(new_str)
+        return new_str
+
+
+
+
+
+
