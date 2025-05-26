1670d463ea0411a1ed7942defb986aa817e3c772
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-05-26 22:16:52 +0530

    Time: 80 ms (55.21%), Space: 19.1 MB (71.96%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,42 +1,47 @@
 class Solution(object):
-    def maxProfit(self, p):
+    def maxProfit(self, prices):
         """
         :type prices: List[int]
         :rtype: int
-        """
-        # brute force approach
-        # TLE
-        # max_p=0
-        # for i in range(0,len(p)):
-        #     for j in range(i+1,len(p)):
-        #         print(max_p,i,j)
-        #         if p[i]<p[j] and max_p<abs((p[i]-p[j])):
-        #             max_p=abs((p[i]-p[j]))
-        #             print(max_p)
-        # return max_p
-        # -----------------------------------------------------
-
-        # just keep 2 vars max_p and min_index_value
-        # if len(p) ==0:
+        # """
+        # max_profit=0
+        # ptr=0
+        # i=1
+        # temp=i
+        # while ptr<len(prices):
+        #     res=prices[ptr]-prices[i]
+        #     max_profit=max(max_profit,res)
+        #     if i==len(prices)-1:
+        #         temp+=1
+        #         i=temp
+        #     i+=1
+        #     ptr+=1
+        # if max_profit<0:
         #     return 0
-        # max_p=0
-        # min_index_value=p[0]
+        # return max_profit
+        # greg hog approach
+        max_profit=0
+        min_price=float('inf')
+        for current_val in prices:
+            if current_val<min_price:
+                min_price=current_val
+            
+            profit = current_val-min_price
 
-        # for i in p:
-        #     if i<min_index_value:
-        #         min_index_value=i
-        #     max_p=max(abs(min_index_value-i),max_p)
-        # # print(max_p) 
-        # return max_p   
-        # if len(p) ==0:
-            # return 0
-        #-------------------------------------------------- 
-        # this is DP -approach
-        max_p=0
-        cur_min_val=p[0]
+            max_profit=max(profit,max_profit)
 
-        for i in p :
-            curr_p=i-cur_min_val
-            max_p=max(max_p,curr_p)
-            cur_min_val=min(cur_min_val,i)
-        return max_p
+        return max_profit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
