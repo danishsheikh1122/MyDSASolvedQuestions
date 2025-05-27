9fa875f180894bb19fb11c5e1a86e316fb78cf0f
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-05-28 01:05:01 +0530

    Time: 8 ms (83.41%), Space: 15.9 MB (96.12%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,61 +1,30 @@
 class Solution(object):
-    def merge(self, nums):
+    def merge(self, intervals):
         """
-        :type i: List[List[int]]
+        :type intervals: List[List[int]]
         :rtype: List[List[int]]
         """
-        # if len(i)==1:
-        #     return i
-
-        # temp=i[0]
-        # to_push=[]
-        # # print(temp)
-        # for n in range(1,len(i)):
-        #     # print(i[n][0])
-        #     if(temp[0] <= i[n][0] and (temp[1]<=i[n][1] and temp[1]>=i[n][0])):
-        #         # print(temp,n)
-        #         temp=[temp[0],i[n][1]]
-        #         to_push.append(temp)
-        #         # print(temp,n)
-        #     #     temp=n
+        # intervals.sort(key=lambda interval:interval[0])
+        # new_li=[]
+        # for interval in intervals:
+        #     if not new_li or new_li[-1][1] < interval[0]:
+        #         new_li.append(interval)
         #     else:
+        #         new_li[-1]=[new_li[-1][0],max(new_li[-1][1],interval[1])]
+        # return new_li
 
-        #         if(temp not in to_push):
-        #             to_push.append(temp)
-        #         to_push.append(i[n])
-        # # print(to_push)
+        #  onlogn on 
 
-        # return to_push
+        intervals.sort(key=lambda x:x[0])
+        prev=intervals[0]
+        res=[]
 
-
-        
-        #worng algorithm 
-
-
-
-
-
-        li=[]
-        
-        nums.sort()
-
-        for i in range(len(nums)):
-            if not li or nums[i][0]>li[-1][1]:
-                li.append(nums[i])
+        for curr_int in intervals:
+            if curr_int[0]<=prev[1]:
+                prev[1]=max(curr_int[1],prev[1])
             else:
-                li[-1][1]=max(li[-1][1],nums[i][1])
-        return li
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                res.append(prev)
+                prev=curr_int
+        
+        res.append(prev)
+        return res
