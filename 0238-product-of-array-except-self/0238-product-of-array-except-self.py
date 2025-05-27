42c42839b60f552504d0b1eebb249143b205ceb9
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-05-27 18:33:28 +0530

    Time: 31 ms (67.48%), Space: 20.2 MB (84.05%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -5,21 +5,53 @@ class Solution(object):
         :rtype: List[int]
         """
         # login in free foam
-        l=[1]
-        r=[1] *len(nums)
-        j=0
-        for i in range(len(nums)-1):
-            l.append(l[i]*nums[i])
-            j+=1
-        j=len(nums)-2
-        for i in range(len(nums)-1,-1,-1):
-            r[j]=r[j+1]*nums[i]
-            j-=1
-        r[len(nums)-1]=1
-        print(r)
+        # l=[1]
+        # r=[1] *len(nums)
+        # j=0
+        # for i in range(len(nums)-1):
+        #     l.append(l[i]*nums[i])
+        #     j+=1
+        # j=len(nums)-2
+        # for i in range(len(nums)-1,-1,-1):
+        #     r[j]=r[j+1]*nums[i]
+        #     j-=1
+        # r[len(nums)-1]=1
+        # print(r)
 
+        # for i in range(len(nums)):
+        #     l[i]=l[i]*r[i]
+        # return l
+        # new approach from soultions
+        res_arr=[1]*len(nums)
+        l=1
         for i in range(len(nums)):
-            l[i]=l[i]*r[i]
-        return l
+            res_arr[i]*=l
+            l*=nums[i]
+        r=1
+        for i in range(len(nums)-1,-1,-1):
+            res_arr[i]*=r
+            r*=nums[i]
+        return res_arr
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     
