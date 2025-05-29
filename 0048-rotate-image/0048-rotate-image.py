d7063193699fb6b83d6aef0defe0610f4a476b32
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-05-29 23:25:34 +0530

    Time: 0 ms (100%), Space: 12.6 MB (18.25%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -4,12 +4,11 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        n = len(matrix)
-        # Transpose the matrix
-        for i in range(n):
-            for j in range(i + 1, n):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        # Reverse each row
+        len_arr=len(matrix)
+
+        for i in range(0,len_arr):
+            for j in range(i+1,len_arr):
+                matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
         for row in matrix:
             row.reverse()
         
