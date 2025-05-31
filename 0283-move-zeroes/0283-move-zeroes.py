613289248deeeae75717602a4bf6c5f6a8130a25
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-05-31 18:54:31 +0530

    Time: 11 ms (35.74%), Space: 13.6 MB (58.46%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -12,29 +12,31 @@ class Solution(object):
         # while ptr<len(nums):
         #     nums[ptr]=0
         #     ptr+=1
-        
-        i=0
-        arr=[]
+
+
+        # my Brute force on on 
+        # i=0
+        # arr=[]
+        # for j in range(len(nums)):
+        #     if nums[j]!=0:
+        #         arr.append(nums[j])
+        #         i+=1
+        # for j in range(len(nums)):
+        #     if j<i:
+        #         nums[j]=arr[j]
+        #     else:
+        #         nums[j]=0
+        if len(nums)==0:
+            return nums
+        i=0 
         for j in range(len(nums)):
             if nums[j]!=0:
-                arr.append(nums[j])
+                nums[i],nums[j]=nums[j],nums[i]
                 i+=1
-        for j in range(len(nums)):
-            if j<i:
-                nums[j]=arr[j]
-            else:
-                nums[j]=0
-       
-        
             
 
-
-
-
-
-
-
-
+        
+            
 
 
 
