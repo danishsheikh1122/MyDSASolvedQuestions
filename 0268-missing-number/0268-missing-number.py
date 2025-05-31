4bc9241d9cd6bb6f2ca6c9ed313a31ac9bbfdb7d
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-05-31 19:28:33 +0530

    Time: 2631 ms (12.28%), Space: 13.5 MB (12.57%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,63 +1,10 @@
 class Solution(object):
-    def missingNumber(self, n):
+    def missingNumber(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        # brute force worst solution posible
-
-        # minn=0
-        # maxx=max(n)+1
-
-        # for i in range(maxx+1):
-        #     if(i not in n):
-        #         return i
-        # return 0
-        # TC=O^2
-        # SC=O(1)
-
-# ----------------------------
-# passed 110/122 great
-
-        # sort arr first
-        # the run a loop if diff between i and i+1==1 continue
-        # else :
-        #     return i
-
-        # n.sort()
-        # move_backward=0
-        # for i in range(1,len(n)):
-        #     if(abs(n[move_backward]-n[i])==1):
-        #         move_backward+=1
-        #         continue
-        #     else:
-        #         return i
-        # if(len(n)!=1):        
-        #     return len(n)
-        # else:
-        #     for i in range(0,len(n)+1):
-        #         if(abs(n[move_backward]-n[i])==1):
-        #             move_backward+=1
-        #             continue
-        #         else:
-        #             return i
-
-
-        # -------------------------------------
-        # using a formula to calc sum as
-        # sum of n numbers = (n(n+1))/2
-        # lenn=len(n)
-        # res=(lenn*(lenn+1)) // 2
-        # summ=sum(n)
-        # return res-summ
-        # -------------------------------------
-        # bit manipulation approach
-
-        xor1=xor2=0
-
-        for i in range(0,len(n)):
-            xor1^=n[i]
-            xor2^=i+1
-        return xor1^xor2
-
-
+        for i in range(0,len(nums)+1):
+            if i not in nums:
+                return i
+        
