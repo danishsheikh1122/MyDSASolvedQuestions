7d031b35bc3f25103639c8f11e636d57f0e0ee24
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-06-01 00:12:56 +0530

    Time: 3 ms (61.92%), Space: 13.5 MB (30.89%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -13,10 +13,25 @@ class Solution(object):
 
         # optimal approach
         # by using sum of natural numbers, use formula to calc sum of n natural numbers 
-        summ=(len(nums)*(len(nums)+1))/2
+        # summ=(len(nums)*(len(nums)+1))/2
 
-        sum_nums=0
-        for num in nums:
-            sum_nums+=num
-        return summ-sum_nums 
+        # sum_nums=0
+        # for num in nums:
+        #     sum_nums+=num
+        # return summ-sum_nums 
+
+
+        # by using bitwise operator xor
+        # res=nums[0]
+        # for i in range(1,len(nums)+1):
+        #     res^=nums[i]
+        # return res
+
+        # best optimal and logical solutionm 
+        res=len(nums)
+        for i in range(len(nums)):
+            res+=i-nums[i]
+        return res
+
+
 
