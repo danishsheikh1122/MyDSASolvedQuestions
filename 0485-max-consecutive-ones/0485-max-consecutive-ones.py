c23b4dcc366e81b6c3e6d6de6a2968fd7389f35d
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-06-01 01:26:11 +0530

    Time: 43 ms (18.42%), Space: 16 MB (35.4%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.py
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.py
@@ -4,13 +4,29 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        m_count=count=0
-        for i in nums:
-            if i ==0:
-                count=0
-            else:
-                count+=1
-                m_count=max(m_count,count)
-        return m_count
+        # full working logic with conditional statements
+        # m_count=count=0
+        # for i in nums:
+        #     if i ==0:
+        #         count=0
+        #     else:
+        #         count+=1
+        #         m_count=max(m_count,count)
+        # return m_count
 
-        
+        # full working without conditional statements
+        # max_count = 0
+        # current_count = 0
+        # for num in nums:
+        #     current_count = current_count * num + num # here is the logic 
+        #     max_count = max(max_count, current_count)
+        # return max_count
+        count=0
+        max_count=0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                count+=1
+                max_count=max(max_count,count)
+            elif nums[i]==0:
+                count=0
+        return max_count
