4b26a78ba45a0f53e63441f0f05859dac70cc346
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-05-31 11:32:50 +0530

    Time: 10 ms (38.57%), Space: 13.6 MB (35.84%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,50 +1,41 @@
 class Solution(object):
-    def moveZeroes(self, n):
+    def moveZeroes(self, nums):
         """
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        #worst approach
-        # arr_len=len(n)-1
-        # new_li=[x for x in n if x!=0]
-        # # print(new_li)
-        # for i in range(len(new_li)-1,arr_len):
-        #     new_li.append(0)
-        # # print(new_li)
-
-        # for i in range(0,len(new_li)):
-        #     n[i]=new_li[i]
-
-
-        # in this approach we used while loop to reverse 
-
-        # count_zeros=0
-        # move_backward=0
-
-        # for i in range(len(n)):
-        #     if n[i]!=0:
-        #         n[move_backward]=n[i]
-        #         move_backward+=1
-        #     else:
-        #         count_zeros+=1
-        # k=move_backward-1      
-        # i=len(n)-1
-        # while k!=i:
-        #     n[i]=0
-        #     i-=1
-        # print(n)
-
-        # in this approach we use efficient approach
-
-
-        count_zeros=0
-        move_backward=0
-
-        for i in range(len(n)):
-            if n[i]!=0:
-                n[move_backward]=n[i]
-                move_backward+=1
+        # ptr=0
+        # for i in range(len(nums)):
+        #     if nums[i]!=0:
+        #         nums[ptr]=nums[i]
+        #         ptr+=1
+        # while ptr<len(nums):
+        #     nums[ptr]=0
+        #     ptr+=1
+        
+        i=0
+        arr=[]
+        for j in range(len(nums)):
+            if nums[j]!=0:
+                arr.append(nums[j])
+                i+=1
+        for j in range(len(nums)):
+            if j<i:
+                nums[j]=arr[j]
             else:
-                count_zeros+=1
-        for i in range(move_backward,move_backward+count_zeros):
-            n[i]=0
+                nums[j]=0
+       
+        
+            
+
+
+
+
+
+
+
+
+
+
+
+
