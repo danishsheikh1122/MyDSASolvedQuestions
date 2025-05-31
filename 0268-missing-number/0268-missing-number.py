4c35ebdb8deeb733d5ed43352e0af29a65bffcf6
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-05-31 23:58:16 +0530

    Time: 0 ms (100%), Space: 13.3 MB (88.95%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -4,11 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # brute force by me 
+        # brute force by me on2
 
         # for i in range(0,len(nums)+1):
         #     if i not in nums:
         #         return i
         
 
-        
+        # optimal approach
+        # by using sum of natural numbers, use formula to calc sum of n natural numbers 
+        summ=(len(nums)*(len(nums)+1))/2
+
+        sum_nums=0
+        for num in nums:
+            sum_nums+=num
+        return summ-sum_nums 
+
