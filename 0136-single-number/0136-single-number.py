898cdcb1c514c9f3525c59b380763a8a7ac07bb5
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-06-02 18:58:38 +0530

    Time: 14 ms (28.3%), Space: 15.2 MB (6.83%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -4,9 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        res=nums[0]
-        for i in range(1,len(nums)):
-            res^=nums[i]
-        return res
+        # brute force mine
+        di={}
+        for i in nums:
+            di[i]=1 if i not in di else di[i]+1
+        
+        for key,value in di.items():
+            if value==1:
+                return key
+
+
+        # res=nums[0]
+        # for i in range(1,len(nums)):
+        #     res^=nums[i]
+        # return res
 
         
