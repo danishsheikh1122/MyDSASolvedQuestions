6681e57e99234537680c018c3ab883df3fc0c549
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-06-03 11:22:34 +0530

    Time: 3 ms (21.49%), Space: 12.5 MB (11.84%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,4 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        # Fuckkkkkkk lol
+        # nums.sort()
+
+        # counting sort algorithm
+        # first count all the numbers r w b and then put it in the nums arr
+
+        num_r=num_w=num_b=0
+        for num in nums:
+            if num==0:
+                num_r+=1
+            elif num==1:
+                num_w+=1
+            else:
+                num_b+=1
+        
+        nums[:num_r]=[0]*num_r
+        nums[num_r:num_r+num_w]=[1]*num_w
+        nums[num_r+num_w:]=[2]*num_b
+        print(nums)
+
