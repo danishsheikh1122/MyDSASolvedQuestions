42a9d4115008a9f60e141885ae339223f793c0aa
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-06-03 20:35:18 +0530

    Time: 13 ms (36.83%), Space: 13.8 MB (16.86%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -5,18 +5,36 @@ class Solution(object):
         :rtype: int
         """
         # brute fore by me 
-        di=Counter(nums)
-        max_number=0
-        res=0
-        print(di)
+        # di=Counter(nums)
+        # max_number=0
+        # res=0
+        # print(di)
         
-        for key,value in di.items():
-            if value>max_number and value>=(len(nums)/2) :
-                print(key,value)
-                max_number=value
-                res=key
+        # for key,value in di.items():
+        #     if value>max_number and value>=(len(nums)/2) :l
+        #         print(key,value)
+        #         max_number=value
+        #         res=key
 
-        return res
+        # return res
+        
+        # most optimal solutions can be formed by using moors voting algorithm 
+        # how many time you'll revise this danish
+        # dumbass 
+
+        current_elem=0
+        count=0
+        for num in nums:
+            if count==0:
+                current_elem=num
+                count+=1
+            elif current_elem==num:
+                count+=1
+            else:
+                count-=1
+        return current_elem
+
+
 
 
         
