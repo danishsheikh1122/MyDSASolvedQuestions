4bd364535deac4ba128c90792a6b6ec1cb509b39
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-06-06 01:02:18 +0530

    Time: 92 ms (59.44%), Space: 44.9 MB (67.14%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
@@ -4,48 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        # BRUTE FORCE approach is to make 2 seperate li of +ve and -ve and run a loop to 
-        # make final res
-
-        # pos=[x for x in nums if x>=0]
-        # neg=[x for x in nums if x<0]
-        # res_li=[]
-        # i=0
-        # while i!=len(pos):
-        #     res_li.append(pos[i])
-        #     res_li.append(neg[i])
-        #     i+=1
-        # # print(res_li)
-        # return res_li
-        # tc-on sc-on
-        # -----------------------------------
-        # same approach but kind of in place
-
-
-        # pos=[x for x in nums if x>=0]
-        # neg=[x for x in nums if x<0]
-        # for i in range(0,len(nums)/2):
-        #     nums[i*2]=pos[i]
-        #     nums[i*2+1]=neg[i]
-        # return nums
-        # tc-on sc-on
-
-        # this takes 2 pass as using 2 loops make it in one pass and sc=on
-
-        # -----------------------------------
-        # one pass ,tc and sc=on most optimal
-        pos=0
-        neg=1
-        res_li=[0]*len(nums)
-        for i in nums:
-            if i >=0:
-                res_li[pos]=i
-                pos+=2
-            else:# i >=0:
-                res_li[neg]=i
-                neg+=2
-        # print(res_li)
-        return res_li   
-        # most optimal 
-        # one pass and tc-on sc on
-        # beats 96.81 and 57.14 on runtmie and space resp
+        pos_arr=[x for x in nums if x>=0]
+        neg_arr=[x for x in nums if x<0]
+        # print(pos_arr,neg_arr)
+        count_nums=0
+        count_arr=0
+        for i in range(len(nums)/2):
+            nums[count_nums]=pos_arr[count_arr]
+            count_nums+=1
+            nums[count_nums]=neg_arr[count_arr]
+            count_nums+=1
+            count_arr+=1
+        return nums
+        
