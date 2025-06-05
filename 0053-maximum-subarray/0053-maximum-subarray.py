563f59113d9a65366d237a5fbf081fc38abe422e
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-06-05 22:07:26 +0530

    Time: 59 ms (71.13%), Space: 21.9 MB (27.05%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,23 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # BRUTE FORCE -TLE
-        # max_sum=float('-inf')
-        # for i in range(0,len(nums)):
-        #     for j in range(i+1,len(nums)+1):
-        #         summ=sum(nums[i:j])
-        #         max_sum=max(summ,max_sum)
+        # max_sum=nums[0]
+        # for i in range(len(nums)):
+        #     res=0
+        #     for j in range(i+1,len(nums)):
+        #         res+=nums[j]
+        #         max_sum=max(max_sum,res)
+
         # return max_sum
-        
-        # --------------------------------------
-        # KADAN'S ALGO
-        curr_sum=0
-        max_sum=float('-inf')
+        if len(nums)==1:
+            return nums[0]
+        max_sum=nums[0]
+        sum=0
 
-        for i in nums:
-            curr_sum+=i
-            max_sum=max(max_sum,curr_sum)
-            if curr_sum<0:
-                curr_sum=0
+        for num in nums:
+            if sum<0:
+                sum=0
+            sum+=num
+            max_sum=max(max_sum,sum)
         return max_sum
-
