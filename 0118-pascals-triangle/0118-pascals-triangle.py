1cd83fa55d694e53de0f098e710da63e3c398b5d
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-06-11 19:09:02 +0530

    Time: 0 ms (100%), Space: 12.5 MB (21.96%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -4,25 +4,14 @@ class Solution(object):
         :type numRows: int
         :rtype: List[List[int]]
         """
-        li=[]
+        resArr=[[1]]
+        for i in range(numRows-1):
+            temp=[0]+resArr[-1]+[0]
+            row=[]
+            for j in range(len(resArr[-1])+1):
+                row.append(temp[j]+temp[j+1])
+            resArr.append(row)
+        return resArr
+
+
         
-        if numRows==0:
-            return li
-        f_row=[1]
-        li.append(f_row)
-        
-        for i in range(1,numRows):
-            prev_row=li[i-1]
-            curr_row=[1]
-
-            for j in range(1,i):
-                # print(prev_row[j-1],prev_row[j])
-                res=prev_row[j-1]+prev_row[j]
-                curr_row.append(res)
-
-            curr_row.append(1)
-            li.append(curr_row)
-
-        return li
-
-
