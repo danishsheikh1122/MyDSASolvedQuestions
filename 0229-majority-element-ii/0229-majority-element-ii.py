b73855be9256974c4118f893a539f24b2aaf613f
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-06-11 23:55:28 +0530

    Time: 18 ms (19.38%), Space: 18.2 MB (7.8%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -7,14 +7,26 @@ class Solution(object):
         di=Counter(nums)
         resArr=[]
         count=0
-        if len(nums)<3:return list(set(nums))
+        # if len(nums)<3:return list(set(nums))
 
         for k,v in di.items():
-            if v>(len(nums)/3):
+            if v>(len(nums)//3):
                 resArr.append(k)
             else:
                 count+=1
         return resArr
+
+        # resArr=[]
+        # for i in range(len(nums)):
+        #     count=0
+        #     for j in range(len(nums)):
+        #         if i!=j:
+        #             if nums[i]==nums[j]:
+        #                 count+=1
+        #         if count>(len(nums)/3):
+        #             resArr.append(nums[i])
+        # return resArr
+
 
             
         
