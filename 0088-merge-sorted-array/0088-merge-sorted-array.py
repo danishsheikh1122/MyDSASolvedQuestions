49d7582ce0ed4b6f9cfe6e3f0422502cd0023b1f
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-06-17 00:40:37 +0530

    Time: 0 ms (100%), Space: 12.5 MB (53.23%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -15,12 +15,28 @@ class Solution(object):
         #         # print(l_nums2)
         # # print(nums1)
         # nums1.sort()
+
         # bute force approach 
         
-        for i in range(n):
-            nums1[m+i]=nums2[i]
-        nums1.sort()
-        return nums1
+        # for i in range(n):
+        #     nums1[m+i]=nums2[i]
+        # nums1.sort()
+        # return nums1
+
+        # optimal 
+        last=m+n-1
+        while m>0 and n>0:
+            if nums1[m-1]>nums2[n-1]:
+                nums1[last]=nums1[m-1]
+                m-=1
+            else:
+                nums1[last]=nums2[n-1]
+                n-=1
+            last-=1
+        while n>0:
+            nums1[last]=nums2[n-1]
+            n-=1
+            last-=1
 
 
 
