3278950a75688a0ddbc99309e6f1a634c4210a6f
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-06-22 11:52:07 +0530

    Time: 0 ms (100%), Space: 12.4 MB (52.64%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -4,30 +4,85 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
-        res = []
-        top, bottom = 0, len(matrix) - 1
-        left, right = 0, len(matrix[0]) - 1
-        res_arr=[]
+        # res = []
+        # top, bottom = 0, len(matrix) - 1
+        # left, right = 0, len(matrix[0]) - 1
+        # res_arr=[]
     
+        # while top<=bottom and left<=right:
+        #     # for top row
+        #     for i in range(left,right+1):
+        #         res_arr.append(matrix[top][i])
+        #     top+=1
+        #     # for right column 
+        #     for i in range(top,bottom+1):
+        #         res_arr.append(matrix[i][right])
+        #     right-=1
+
+        #     if top<=bottom:
+        #         for i in range(right,left-1,-1):
+        #             res_arr.append(matrix[bottom][i])
+        #         bottom-=1
+        #     if left<=right:
+        #         for i in range(bottom,top-1,-1):
+        #             res_arr.append(matrix[i][left])
+        #         left+=1
+        # return res_arr
+        res=[]
+        left=0
+        top=0
+        right=len(matrix[0])-1#rows
+        bottom=len(matrix)-1#cols
         while top<=bottom and left<=right:
-            # for top row
+            # top row
             for i in range(left,right+1):
-                res_arr.append(matrix[top][i])
+                res.append(matrix[top][i])
             top+=1
-            # for right column 
+
+            # right col
             for i in range(top,bottom+1):
-                res_arr.append(matrix[i][right])
+                res.append(matrix[i][right])
             right-=1
 
-            if top<=bottom:
+            # bottom row
+            if(top<=bottom):
                 for i in range(right,left-1,-1):
-                    res_arr.append(matrix[bottom][i])
+                    res.append(matrix[bottom][i])
                 bottom-=1
-            if left<=right:
-                for i in range(bottom,top-1,-1):
-                    res_arr.append(matrix[i][left])
-                left+=1
-        return res_arr
-        
             
+            # left col
+            if (left<=right):
+                for i in range(bottom,top-1,-1):
+                    res.append(matrix[i][left])
+            left+=1
+
+        return res
+
         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
