73e17ee44a3aa0a164b4f81aa93d9d7030fdcc04
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-06-26 10:38:42 +0530

    Time: 38 ms (7.24%), Space: 12.5 MB (96.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,4 @@
+from collections import Counter
 class Solution(object):
     def isAnagram(self, s, t):
         """
@@ -5,16 +6,28 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        # if len(s)!=len(t):
+        #     return False
+        # di={}
+        # for c in s:
+        #     di[c]=di[c]+1 if c in di else 1
+        # print(di)
+        # for c in t:
+        #     if c in di and di[c]>0:
+        #         di[c]-=1
+        #     else:
+        #         return False
+        
+        # return True
+        # # on on
+        # brute force 
         if len(s)!=len(t):
             return False
-        di={}
-        for c in s:
-            di[c]=di[c]+1 if c in di else 1
-        print(di)
-        for c in t:
-            if c in di and di[c]>0:
-                di[c]-=1
+        
+        di=Counter(s)
+        for char in t:
+            if char in di and di[char]!=0:
+                di[char]-=1
             else:
                 return False
-        
         return True
