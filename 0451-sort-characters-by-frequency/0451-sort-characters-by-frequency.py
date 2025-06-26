936638c29fb0bb05080c95b72f58a75a0bde9cc4
PFG DANiSH <144151435+danishsheikh1122@users.noreply.github.com>
2025-06-26 12:14:15 +0530

    Time: 27 ms (38.71%), Space: 13.4 MB (71.27%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -4,13 +4,31 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        di = Counter(s)  
-        dii = sorted(di.items(), key=lambda item: (-item[1],item[0])) 
+        # di = Counter(s)  
+        # dii = sorted(di.items(), key=lambda item: (-item[1],item[0])) 
         # print(dii)
 
-        new_str = ""
-        for key, value in dii:
-            new_str += key * value 
-        return new_str
+        # new_str = ""
+        # for key, value in dii:
+            # new_str += key * value 
+        # return new_str
+        di=Counter(s)
+        di=sorted(di.items(),key=lambda item:(-item[1],item[0]))
+        resStr=""
+        for key,value in di:
+            resStr+=key*value
+        return resStr
         
+
+
+
+
+
+
+
+
+
+
+
+
 
